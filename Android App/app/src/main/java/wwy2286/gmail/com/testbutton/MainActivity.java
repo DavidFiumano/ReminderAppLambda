2dbb77e481e9fc43921474606c0dc5dff4324150
William <wwy2286@gmail.com>
2019-11-05 17:25:38 -0500

    added alarm

--- Android App/app/src/main/java/wwy2286/gmail/com/testbutton/MainActivity.java
+++ Android App/app/src/main/java/wwy2286/gmail/com/testbutton/MainActivity.java
@@ -1,5 +1,10 @@
 package wwy2286.gmail.com.testbutton;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -18,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(getApplicationContext(), "Button works toast", Toast.LENGTH_SHORT);
         System.out.println("button works");
         toast.show();
+
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        intent.putExtra("NotificationText", "some text");
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, 20000, pendingIntent);
+    }
+
+    public class AlarmReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            // create notification here
+        }
     }
 }
--- Task Manager/TaskManager/app/src/main/java/com/example/taskmanager/SecondActivity.java
+++ Task Manager/TaskManager/app/src/main/java/com/example/taskmanager/SecondActivity.java
@@ -1,22 +1,23 @@
 package com.example.taskmanager;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 
 public class SecondActivity extends AppCompatActivity {
     TextView name, email;
@@ -70,13 +71,48 @@ public class SecondActivity extends AppCompatActivity {
     }
 
     private void signOut() {
-        mGoogleSignInClient.signOut()
-                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(SecondActivity.this, "Signed out successfully!", Toast.LENGTH_LONG).show();
-                        finish();
-                    }
-                });
+//        mGoogleSignInClient.signOut()
+//                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        Toast.makeText(SecondActivity.this, "Signed out successfully!", Toast.LENGTH_LONG).show();
+//                        finish();
+//                    }
+//                });
+
+        Intent intent = new Intent(getApplicationContext(), MyAlarmReceiver.class);
+        final PendingIntent pIntent = PendingIntent.getBroadcast(this, MyAlarmReceiver.REQUEST_CODE,
+                intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Setup periodic alarm every every half hour from this point onwards
+        long firstMillis = System.currentTimeMillis(); // alarm is set right away
+        AlarmManager alarm = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
+        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
+        alarm.setExact(AlarmManager.RTC_WAKEUP, 1000, pIntent);
+    }
+
+    public class MyTestService extends IntentService {
+        public MyTestService() {
+            super("MyTestService");
+        }
+
+        @Override
+        protected void onHandleIntent(Intent intent) {
+            // Do the task here
+            Log.i("MyTestService", "Service running");
+        }
+    }
+
+    public class MyAlarmReceiver extends BroadcastReceiver {
+        public static final int REQUEST_CODE = 12345;
+        public static final String ACTION = "com.codepath.example.servicesdemo.alarm";
+
+        // Triggered by the Alarm periodically (starts the service to run task)
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent i = new Intent(context, MyTestService.class);
+            i.putExtra("foo", "bar");
+            context.startService(i);
+        }
     }
 }
